1a786e3e1b35811afd5426d132abc6a179f766b7
Radhika Bitla <bitlaradhika3@gmail.com>
2023-04-13 18:53:44 +0530

    done done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
+ //   System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
@@ -16,3 +16,26 @@ public class Main {
     System.out.println(p.getBill());
   }
 }
+/*
+Veg pizza base price = 300
+Non-veg pizza base price = 400
+Extra Cheese Price = 80
+Extra Toppings For Veg Pizza = 70
+Extra Toppings For Non-veg Pizza = 120
+Paper bag Price = 20
+
+Example 1:
+
+Base Price Of The Pizza: 300
+Extra Cheese Added: 80
+Extra Toppings Added: 70
+Paperbag Added: 20
+Total Price: 470
+
+Example 2:
+Base Price Of The Pizza: 400
+Extra Cheese Added: 80
+Extra Toppings Added: 120
+Total Price: 600
+
+ */
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,83 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int totalBill=0;
+
+    private int cheesePrice=80;
+
+    private int cheeseBill=0;
+    private int toppingsBill=0;
+
+    private int ToppingsPrice=120;
+
+    private boolean takeAway=false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg==true)
+        {
+            this.price=300;
+            this.ToppingsPrice=70;
+        }
+        else {
+            this.price=400;
+            this.ToppingsPrice=120;
+        }
     }
 
-    public int getPrice(){
+    public int getPrice()
+    {
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese()
+    {
+        cheeseBill+=this.cheesePrice;
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addExtraToppings()
+    {
+        toppingsBill+=this.ToppingsPrice;
+    //    System.out.println(ToppingsPrice);
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void addTakeaway()
+    {
+        takeAway=true;
     }
 
     public String getBill(){
-        // your code goes here
+        this.bill=doBilling();
         return this.bill;
     }
+    public String doBilling()
+    {
+        int totalBill=0;
+        String result="";
+        totalBill+=price;
+        result=result+"Base Price Of The Pizza: "+String.valueOf(this.price)+"\n";
+            if(this.cheeseBill!=0)
+            {
+                totalBill+=this.cheeseBill;
+                result=result+"Extra Cheese Added: "+String.valueOf(this.cheeseBill)+"\n";
+            }
+            if(this.toppingsBill!=0)
+            {
+                totalBill+=this.toppingsBill;
+                result=result+"Extra Toppings Added: "+String.valueOf(this.toppingsBill)+"\n";
+            }
+            if(takeAway==true)
+            {
+                totalBill+=20;
+                result=result+"Paperbag Added: "+String.valueOf(20)+"\n";
+            }
+            result=result+"Total Price: "+String.valueOf(totalBill);
+            return result;
+    }
 }
+/*
+Base Price Of The Pizza: 300
+Extra Cheese Added: 80
+Extra Toppings Added: 70
+Paperbag Added: 20
+Total Price: 470
+*/

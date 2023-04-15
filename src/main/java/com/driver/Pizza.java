3ff6c0fe06f2e0b7ec4ce46a864c72b990f32152
Radhika Bitla <bitlaradhika3@gmail.com>
2023-04-15 14:09:54 +0530

    done

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,87 +2,88 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
+private int price;//final price
+    private int vegBasePrice = 300;
+    private int nonVegBasePrice = 400;
+    private int basePrice;
+    private boolean isVeg;
     private String bill;
+    private boolean isCheeseAdded;
+    private int cheesePrice = 80;
+    private boolean isToppingAdded;
+    private int vegToppingPrice = 70;
+    private int nonVegToppingPrice = 120;
+    private int toppingPrice;
+    private boolean isTakeAway;
+    private int takeAwayPrice = 20;
 
-    private int totalBill=0;
-
-    protected int cheesePrice=80;
-
-    private int cheeseBill=0;
-    private int toppingsBill=0;
-
-    protected int ToppingsPrice=120;
-
-    private boolean takeAway=false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        if(isVeg==true)
-        {
-            this.price=300;
-            this.ToppingsPrice=70;
-        }
-        else {
-            this.price=400;
-            this.ToppingsPrice=120;
-        }
+        // your code goes here
+        basePrice = getBasePrice();
+        price = basePrice;
+        isCheeseAdded = false;
+        isToppingAdded = false;
+        isTakeAway = false;
+
     }
 
-    public int getPrice()
-    {
+    public int getPrice(){
         return this.price;
     }
 
-    public void addExtraCheese()
-    {
-        cheeseBill+=this.cheesePrice;
+    private int getBasePrice(){
+        basePrice = isVeg ? vegBasePrice : nonVegBasePrice;
+        return basePrice;
     }
 
-    public void addExtraToppings()
-    {
-        toppingsBill+=this.ToppingsPrice;
-    //    System.out.println(ToppingsPrice);
+    public void addExtraCheese(){
+        // your code goes here
+        if(!isCheeseAdded){
+            isCheeseAdded = true;
+            price += cheesePrice;
+        }
+
     }
 
-    public void addTakeaway()
-    {
-        takeAway=true;
+    public void addExtraToppings(){
+        // your code goes here
+        if(!isToppingAdded){
+            this.isToppingAdded = true;
+            if(isVeg) {
+                toppingPrice += vegToppingPrice;
+            }else{
+                toppingPrice += nonVegToppingPrice;
+            }
+            price += toppingPrice;
+        }
+
+    }
+
+    public void addTakeaway(){
+        // your code goes here
+        if(!isTakeAway){
+            isTakeAway = true;
+            price += takeAwayPrice;
+        }
     }
 
     public String getBill(){
-        this.bill=doBilling();
+        // your code goes here
+        String var = "";
+        var += "Base Price Of The Pizza: " + basePrice + "\n";
+        if(isCheeseAdded){
+            var += "Extra Cheese Added: " + cheesePrice + "\n";
+        }
+        if(isToppingAdded){
+            var += "Extra Toppings Added: " + toppingPrice + "\n";
+        }
+        if(isTakeAway){
+            var += "Paperbag Added: " + takeAwayPrice + "\n";
+        }
+        var += "Total Price: " + price + "\n";
+        this.bill = var;
         return this.bill;
     }
-    public String doBilling()
-    {
-        int totalBill=0;
-        String result="";
-        totalBill+=price;
-        result=result+"Base Price Of The Pizza: "+String.valueOf(this.price)+"\n";
-            if(this.cheeseBill!=0)
-            {
-                totalBill+=this.cheeseBill;
-                result=result+"Extra Cheese Added: "+String.valueOf(this.cheeseBill)+"\n";
-            }
-            if(this.toppingsBill!=0)
-            {
-                totalBill+=this.toppingsBill;
-                result=result+"Extra Toppings Added: "+String.valueOf(this.toppingsBill)+"\n";
-            }
-            if(takeAway==true)
-            {
-                totalBill+=20;
-                result=result+"Paperbag Added: "+String.valueOf(20)+"\n";
-            }
-            result=result+"Total Price: "+String.valueOf(totalBill);
-            return result;
-    }
 }
-/*
-Base Price Of The Pizza: 300
-Extra Cheese Added: 80
-Extra Toppings Added: 70
-Paperbag Added: 20
-Total Price: 470
-*/
+
